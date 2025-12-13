78854a05298ba8499d0102461e6d7e9858bf9b80
nezirvishi <118315883+nezirvishi@users.noreply.github.com>
2025-12-13 21:49:53 +0100

    Remove unused assets and clean up styles
    I deleted multiple unused image and model files from the public directory to reduce project size. Cleaned up index.css by removing unused utility and component classes, as well as related custom CSS for grids, marquee, and other elements. Fixed a typo in index.html for the favicon MIME type and improved key usage in AnimatedCounter.jsx.

--- src/components/AnimatedCounter.jsx
+++ src/components/AnimatedCounter.jsx
@@ -4,11 +4,11 @@ import CountUp from 'react-countup'
 
 const AnimatedCounter = () => {
     return (
-        <div id='counter' className='padding-x-lg x:mt-0 mt-32'>
+        <div id='counter' className='padding-x-lg xl:mt-0 mt-32'>
             <div className='mx-auto grid-3-cols'>
                 {counterItems.map((item) => (
-                    <div className='bg-zinc-900 rounded-lg p-10 flex flex-col justify-center'>
-                        <div key={counterItems.label} className='counter-number text-white text-5xl font-bold mb-2'>
+                    <div key={item.label} className='bg-zinc-900 rounded-lg p-10 flex flex-col justify-center'>
+                        <div className='counter-number text-white text-5xl font-bold mb-2'>
                             <CountUp suffix={item.suffix} end={item.value} />
                         </div>
                         <div className='text-white-50 text-lg'>{item.label}</div>
